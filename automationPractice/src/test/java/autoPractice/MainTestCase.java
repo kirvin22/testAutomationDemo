63988a1f4ac6603a52b8c6d9c4c207139085e706
kirvin22 <kiranpd32@gmail.com>
2023-11-21 11:09:59 +0000

    firstPush to github

---
+++ automationPractice/src/test/java/autoPractice/MainTestCase.java
@@ -0,0 +1,17 @@
+package autoPractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainTestCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver =new ChromeDriver();
+		
+		driver.get("google.com");
+
+	}
+
+}
---
+++ automationPractice/src/test/java/autoPractice/MainTestCase2.java
@@ -0,0 +1,17 @@
+package autoPractice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainTestCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver =new ChromeDriver();
+		
+		driver.get("InternetHookUp");
+
+	}
+
+}
